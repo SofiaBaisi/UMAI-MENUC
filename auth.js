6fe2f5507bd3be6706fdf03d49f91e819ec99da5
SofiaBaisi <142254420+SofiaBaisi@users.noreply.github.com>
2024-06-14 19:00:02 -0300

    Add files via upload
    Signed-off-by: SofiaBaisi <142254420+SofiaBaisi@users.noreply.github.com>

---
+++ auth.js
@@ -0,0 +1,56 @@
+// backend/routes/auth.js
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+// Ruta de registro de usuario
+router.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({ email, password });
+    await user.save();
+
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, 'secret', { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Ruta de login de usuario
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, 'secret', { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
+
